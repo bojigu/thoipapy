f9211ed0ac951151a26f60be56eb5a6c63fcc6e8
mark <mark.teese@tum.de>
2020-09-11 14:42:02 +0200

    gather mean_decrease_impurity and mean_decrease_accuracy data together
    MDI needed to be recreated based on final dataset and tuned ML model

--- thoipapy/feature_importance/mean_decrease_impurity.py
+++ thoipapy/feature_importance/mean_decrease_impurity.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 
 from thoipapy.ML_model.tune import tune_ensemble_parameters
-from thoipapy.utils import create_colour_lists, make_sure_path_exists
-from thoipapy.feature_importance.plots import create_var_imp_plot
+from thoipapy.utils import make_sure_path_exists
 from thoipapy.validation.feature_selection import drop_cols_not_used_in_ML
-from thoipapy.ML_model.train_model import return_classifier_with_loaded_ensemble_parameters, THOIPA_classifier_with_settings_deprecated
+from thoipapy.ML_model.train_model import return_classifier_with_loaded_ensemble_parameters
 
 
 def get_initial_ensemble_parameters_before_feature_selection(s, logging):
@@ -67,29 +65,9 @@ def calc_feat_import_using_MDI_before_feature_seln(s, logging):
             logging.info("Feature ranking:")
         else:
             raise ValueError("model type unknown")
-        forest.fit(X, y)
-        importances_arr = forest.feature_importances_
-        std_arr = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
-        indices_arr = np.argsort(importances_arr)[::-1]
 
-        importances_text_list = X.columns.tolist()
+        df_imp = calculate_mean_decrease_impurity_for_dataset(X, y, forest, model_type, logging)
 
-        #order_list = [importances_arr[indices_arr[f]] for f in range(X.shape[1])]
-
-        nested_dict = {}
-
-        for f in range(X.shape[1]):
-            if f < 10 :
-                logging.info("%d. feature %d (%f) %s" % (f + 1, indices_arr[f], importances_arr[indices_arr[f]], importances_text_list[indices_arr[f]]))
-            single_feature_dict = {"original_order" : indices_arr[f], "mean_decrease_impurity{}".format(model_type) : importances_arr[indices_arr[f]], "feature{}".format(model_type) : importances_text_list[indices_arr[f]],  "std{}".format(model_type) : std_arr[f]}
-            nested_dict[f + 1] = single_feature_dict
-
-        sys.stdout.write("\n\n"), sys.stdout.flush()
-
-        df_imp = pd.DataFrame(nested_dict).T
-        df_imp["order_importance{}".format(model_type)] = df_imp.index
-        #df_imp.set_index("feature", inplace=True)
-        df_imp.set_index("original_order", inplace=True)
         output_dfs.append(df_imp)
 
     df_imp2 = pd.concat(output_dfs, axis=1)
@@ -100,29 +78,25 @@ def calc_feat_import_using_MDI_before_feature_seln(s, logging):
     df_imp2.to_csv(mean_decrease_impurity_all_features_csv)
 
 
-def fig_feat_import_from_mean_decrease_impurity(s, logging):
-    """Create figures showing ML feature importance.
+def calculate_mean_decrease_impurity_for_dataset(X, y, forest, model_type, logging):
+    forest.fit(X, y)
+    importances_arr = forest.feature_importances_
+    std_arr = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+    indices_arr = np.argsort(importances_arr)[::-1]
+    importances_text_list = X.columns.tolist()
+    # order_list = [importances_arr[indices_arr[f]] for f in range(X.shape[1])]
+    nested_dict = {}
+    for f in range(X.shape[1]):
+        if f < 10:
+            logging.info("%d. feature %d (%f) %s" % (f + 1, indices_arr[f], importances_arr[indices_arr[f]], importances_text_list[indices_arr[f]]))
+        single_feature_dict = {"original_order": indices_arr[f], "mean_decrease_impurity{}".format(model_type): importances_arr[indices_arr[f]], "feature{}".format(model_type): importances_text_list[indices_arr[f]],
+                               "std{}".format(model_type): std_arr[f]}
+        nested_dict[f + 1] = single_feature_dict
+    sys.stdout.write("\n\n"), sys.stdout.flush()
+    df_imp = pd.DataFrame(nested_dict).T
+    df_imp["order_importance{}".format(model_type)] = df_imp.index
+    # df_imp.set_index("feature", inplace=True)
+    df_imp.set_index("original_order", inplace=True)
+    return df_imp
 
-    Fig1 : Barchart all features
-    Fig2 : Barchart top features (currently set at 30)
 
-    Parameters
-    ----------
-    s : dict
-        Settings dictionary
-    logging : logging.Logger
-        Python object with settings for logging to console and file.
-    """
-    plt.style.use('seaborn-whitegrid')
-    plt.rcParams['errorbar.capsize'] = 1
-    plt.rcParams.update({'font.size':4})
-    colour_dict = create_colour_lists()
-
-    mean_decrease_impurity_all_features_csv = Path(s["thoipapy_data_folder"]) / f"results/{s['setname']}/train_data/01_feat_imp_MDI_before_feature_seln.csv"
-    variable_importance_all_png = Path(s["thoipapy_data_folder"]) / "results" / s["setname"] / "feat_imp/all_var_import.png"
-    variable_importance_top_png = Path(s["thoipapy_data_folder"]) / "results" / s["setname"] / "feat_imp/top_var_import.png"
-
-    df_imp = pd.read_csv(mean_decrease_impurity_all_features_csv, index_col = 0)
-
-    create_var_imp_plot(df_imp, colour_dict, variable_importance_all_png, df_imp.shape[0])
-    #create_var_imp_plot(df_imp, colour_dict, variable_importance_top_png, 30)
--- thoipapy/run.py
+++ thoipapy/run.py
@@ -19,6 +19,7 @@ import warnings
 import thoipapy.experimental_data.add_experimental_data_to_train_set
 import thoipapy.feature_importance.mean_decrease_accuracy
 import thoipapy.feature_importance.mean_decrease_impurity
+import thoipapy.feature_importance.plots
 import thoipapy.features.physical_parameters
 import thoipapy.validation.gather
 from thoipapy.clustering.pairwise_aln_similarity_matrix import create_identity_matrix_from_protein_set
@@ -201,11 +202,11 @@ if __name__ == "__main__":
         if s["run_feature_selection"]:
             thoipapy.feature_importance.mean_decrease_impurity.get_initial_ensemble_parameters_before_feature_selection(s, logging)
             thoipapy.feature_importance.mean_decrease_impurity.calc_feat_import_using_MDI_before_feature_seln(s, logging)
-            thoipapy.feature_importance.mean_decrease_impurity.fig_feat_import_from_mean_decrease_impurity(s, logging)
             thoipapy.feature_importance.remove_duplicates.remove_duplicate_features_with_lower_MDI(s, logging)
             thoipapy.feature_importance.anova.select_best_features_with_anova(s, logging)
             thoipapy.feature_importance.ensemble_rfe.select_best_features_with_ensemble_rfe(s, logging)
             thoipapy.feature_importance.merge.merge_top_features_anova_ensemble(s, logging)
+            thoipapy.feature_importance.plots.plot_feature_importance(s, logging)
 
         if s["tune_ensemble_parameters"]:
             thoipapy.ML_model.tune.tune_ensemble_parameters_after_feature_seln(s, logging)
