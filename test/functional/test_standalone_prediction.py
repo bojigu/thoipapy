db0fd4795989de27c44339054caaee1ef673f101
mark <mark.teese@tum.de>
2020-10-30 23:55:19 +0100

    extend functional tests to include different TMDs

--- test/functional/test_standalone_prediction.py
+++ test/functional/test_standalone_prediction.py
@@ -3,14 +3,28 @@ from pathlib import Path
 import thoipapy
 from test.helpers.helpers import TestProtein
 from thoipapy import run_THOIPA_prediction
-from thoipapy.predict import get_md5_checksum
 
 
-def test_standalone_prediction():
+def test_standalone_prediction_for_tmd_with_few_homologues():
     tp: TestProtein = TestProtein()
     tp.with_BNIP3()
     thoipapy_module_path = Path(thoipapy.__file__).parents[1]
-    out_dir = thoipapy_module_path / "test/test_outputs/test_predict"
+    out_dir = thoipapy_module_path / "test/test_outputs/test_predict/BNIP3"
+
+    run_THOIPA_prediction(tp.protein_name, tp.md5, tp.tmd_seq, tp.full_seq, out_dir, create_heatmap=True)
+
+    assert (out_dir / "datafiles").is_dir()
+    assert (out_dir / "heatmap.pdf").is_file()
+    assert (out_dir / "heatmap.png").is_file()
+    assert (out_dir / "THOIPA_out.csv").is_file()
+    assert (out_dir / "THOIPA_out.xlsx").is_file()
+
+
+def test_standalone_prediction_for_tmd_with_many_homologues():
+    tp: TestProtein = TestProtein()
+    tp.with_4ryiA2()
+    thoipapy_module_path = Path(thoipapy.__file__).parents[1]
+    out_dir = thoipapy_module_path / "test/test_outputs/test_predict/4ryiA2"
 
     run_THOIPA_prediction(tp.protein_name, tp.md5, tp.tmd_seq, tp.full_seq, out_dir, create_heatmap=True)
 
--- test/helpers/helpers.py
+++ test/helpers/helpers.py
@@ -23,12 +23,33 @@ class TestProtein:
         self.tmd_start = m.start()
         self.tmd_end = m.end()
 
-    def with_BNIP3(self):
+    def with_ATP1B1(self):
         self.tmd_seq = "LLFYVIFYGCLAGIFIGTIQVMLLTI"
         self.full_seq = "MARGKAKEEGSWKKFIWNSEKKEFLGRTGGSWFKILLFYVIFYGCLAGIFIGTIQVMLLTISEFKPTYQDRVAPPGLTQIPQIQKTEISFRPNDPKSYEEYVRNIVRFLEKY"
+        self.protein_name = "P05026_ATP1B1"
+        self.acc = "P05026"
+        self.md5 = get_md5_checksum(self.tmd_seq, self.full_seq)
+        m = re.search(self.tmd_seq, self.full_seq)
+        self.tmd_start = m.start()
+        self.tmd_end = m.end()
+
+    def with_BNIP3(self):
+        self.tmd_seq = "VFLPSLLLSHLLAIGLGIYIG"
+        self.full_seq = "RSSSKSSHCDSPPRSQTPQDTNRASETDTHSIGEKNSSQSEEDDIERRKEVESILKKNSDWIWDWSSRPENIPPKEFLFKHPKRTATLSMRNTSVMKKGGIFSAEFLKVFLPSLLLSHLLAIGLGIYIGRRLTTSTSTF"
         self.protein_name = "Q12983_BNIP3"
         self.acc = "Q12983"
         self.md5 = get_md5_checksum(self.tmd_seq, self.full_seq)
         m = re.search(self.tmd_seq, self.full_seq)
         self.tmd_start = m.start()
         self.tmd_end = m.end()
+
+    def with_4ryiA2(self):
+        self.tmd_seq = "PGMTIGMIWAVLFGLIALSVA"
+        self.full_seq = "KKSSIIVFFLTYGLFYVSSVLFPIDRTWYDALEKPSWTPPGMTIGMIWAVLFGLIALSVAIIYNNYGFKPKTFWFLFLLNYIFNQAFSYFQFSQKNLFLATVDCLLVAITTLLLIMFSSNLSKVSAWLLIPYFLWSAFATYLSWTIYSIN"
+        self.protein_name = "4ryiA2_TspO"
+        self.acc = "4ryiA2"
+        self.md5 = get_md5_checksum(self.tmd_seq, self.full_seq)
+        m = re.search(self.tmd_seq, self.full_seq)
+        self.tmd_start = m.start()
+        self.tmd_end = m.end()
+
